78a66502752f09f573ffb5772be7887fd1aca336
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-09-07 12:10:54 +0100

    Tests of using yprev in output

--- tests/testthat/test-difeq-delay.R
+++ tests/testthat/test-difeq-delay.R
@@ -22,3 +22,31 @@ test_that("fib", {
   expect_equal(dim(h2), c(3, 11))
   expect_equal(h2[, 10:11], h, check.attributes = FALSE)
 })
+
+test_that("prev and output", {
+  growth <- function(i, t, y, p) {
+    ret <- y + p
+    attr(ret, "output") <- yprev(i - 1L)
+    ret
+  }
+
+  y0 <- runif(5)
+  p <- runif(5)
+  i <- 0:10
+  i2 <- seq(0, 10, by = 2)
+
+  cmp <- y0 + outer(p, i)
+  cmp2 <- y0 + outer(p, i2)
+
+  res <- difeq(y0, i, growth, p, return_initial = TRUE, n_out = 5,
+               n_history = 2L)
+  expect_equal(res, cmp, check.attributes = FALSE)
+  output <- attr(res, "output")
+  expect_equal(output, cbind(y0, cmp[, -ncol(cmp)], deparse.level = 0))
+
+  res2 <- difeq(y0, i2, growth, p, return_initial = TRUE, n_out = 5,
+                n_history = 2L)
+  expect_equal(res2, cmp2, check.attributes = FALSE)
+  output2 <- attr(res2, "output")
+  expect_equal(output2, output[, i2 + 1])
+})
