e622ce305d06fa2ae7e2e06a0a4945ff565ebad4
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-10-07 19:53:53 +0100

    Drop unused parameter

--- tests/testthat/growth_int.c
+++ tests/testthat/growth_int.c
@@ -2,7 +2,6 @@
 
 void growth(size_t n, size_t i, double *y, double *y_next,
             size_t n_out, double *output, void *data) {
-  double *pars = (double*) data;
   int idx[5] = {0, 1, 2, 3, 4};
   double yprev[5];
   yprev_vec_int(i - 1, idx, 5, yprev);
